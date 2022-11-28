52f71e3a6f9488730d62eb419acdd4b40cf37765
NesterTop1 <m8743939@gmail.com>
2022-11-28 17:40:31 +0300

    text

--- aiogram_test.py
+++ aiogram_test.py
@@ -146,20 +146,32 @@ async def process_connect(message: types.Message, state: FSMContext):
     poll_keyboard.add(types.KeyboardButton(text="Отмена"))
     async with state.proxy() as data:
         data['connect'] = message.text
-        question = md.bold('Заказчик:'), md.text(data['name']), sep='\n', md.text('Проект:'), md.text(data['projected']),md.bold('Способ связи:'), md.text(data['connect'])
-        await message.answer(f"Данные введены верно? {question}" , reply_markup=poll_keyboard)
+        #x = md.text(('Данные введены верно?'),md.bold('Заказчик:'), md.text(data['name']),sep='')
+        q = (md.text('Данные введены верно?'), sep='\n')
+        w = (md.text(md.bold('Заказчик:'), md.text(data['name']), md.text('Проект:', md.text(data['projected'])),
+                            md.text('Способ связи:', data['connect']), sep='\n')
+        #e = md.bold('Проект:'), md.text(data['projected']), sep='\n'
+        #r = md.bold('Способ связи:'), md.text(data['connect']), sep='\n'
+        #question = md.text(
+        #    md.text('Данные введены верно?'),md.bold('Заказчик:'), md.text(data['name']),
+        #    md.bold('Проект:'), md.text(data['projected']),
+        #    md.bold('Способ связи:'), md.text(data['connect']),
+        #    sep='\n')
+        #question = (md.text(md.bold('Заказчик:'), md.text(data['name']), md.text('Проект:', md.text(data['projected'])),
+        #                    md.text('Способ связи:', data['connect']), sep='\n'))
+        await message.answer(f"{q}{w}", reply_markup=poll_keyboard, parse_mode=types.ParseMode.MARKDOWN_V2)
 
     await Form.next()
+#f"Данные введены верно? {question}"
 
 #@dp.message_handler(state=Form.calldata)
 @dp.message_handler(lambda message: message.text == "Да",state=Form.calldata)
 async def action_yes(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['calldata'] = message.text
-        question = (md.text(md.text('Заказчик:', md.text(data['name'])), md.text('Проект:', md.text(data['projected'])),
-                            md.text('Способ связи:', data['connect']), sep='\n'))
+        question = md.text(md.bold('Заказчик:'), md.text(data['name']), md.bold('Проект:'), md.text(data['projected']), md.bold('Способ связи:'), md.text(data['connect']),sep='\n')
         remove_keyboard = types.ReplyKeyboardRemove()
-        await bot.send_message(-1001830422328, f"{question}")
+        await bot.send_message(-1001830422328, f"{question}", parse_mode=types.ParseMode.MARKDOWN_V2)
         await state.finish()
     
     await message.reply("Отправляю в канал!")
