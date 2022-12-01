6ba590f459070dfc0ea6d0c8b307934752de7a24
NesterTop1 <m8743939@gmail.com>
2022-12-01 21:31:33 +0300

    test

--- aiogram_test.py
+++ aiogram_test.py
@@ -32,25 +32,15 @@ HELP_COMMAND = """
 /channel - Наш канал
 """
 
-
-
 bot = Bot(token=TOKEN)
 
 dp = Dispatcher(bot, storage=MemoryStorage())
-
-
-
 
 class Form(StatesGroup):
     name = State()  
     projected = State()
     connect = State()  
     calldata = State()
-    cancel = State()
-
-
-
-
 
 
 
@@ -78,32 +68,8 @@ async def send_channel(message: types.Message):
 
 @dp.message_handler(commands='reg')
 async def cmd_start(message: types.Message):
-    """
-    Conversation's entry point
-    """
-    # Set state
     await Form.name.set()
-
     await message.reply("Как тебя зовут?")
-
-
-# You can use state '*' if you need to handle all states
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    """
-    Allow user to cancel any action
-    """
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-    # Cancel state and inform user about it
-    await state.finish()
-    # And remove keyboard (just in case)
-    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
-
 
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
@@ -115,16 +81,6 @@ async def process_name(message: types.Message, state: FSMContext):
 
     await Form.next()
     await message.reply("Опишите ваш проект")
-
-
-# Check age. Age gotta be digit
-#@dp.message_handler(lambda message: not message.text.isdigit(), state=Form.connect)
-#async def process_age_invalid(message: types.Message):
-#    """
-#    If age is invalid
-#    """
-#    return await message.reply("Age gotta be a number.\nHow old are you? (digits only)")
-
 
 @dp.message_handler(state=Form.projected)
 async def process_name(message: types.Message, state: FSMContext):
@@ -146,7 +102,8 @@ async def process_connect(message: types.Message, state: FSMContext):
     poll_keyboard.add(types.KeyboardButton(text="Отмена"))
     async with state.proxy() as data:
         data['connect'] = message.text
-        question = md.bold('Заказчик:'), md.text(data['name']), sep='\n', md.text('Проект:'), md.text(data['projected']),md.bold('Способ связи:'), md.text(data['connect'])
+        question = (md.text(md.text('Заказчик:', md.text(data['name'])), md.text('Проект:', md.text(data['projected'])),
+                            md.text('Способ связи:', data['connect']), sep='\n'))
         await message.answer(f"Данные введены верно? {question}" , reply_markup=poll_keyboard)
 
     await Form.next()
@@ -168,7 +125,6 @@ async def action_yes(message: types.Message, state: FSMContext):
     
 
 
-#@dp.message_handler(state=Form.calldata)
 @dp.message_handler(lambda message: message.text == "Отмена",state=Form.calldata)
 async def action_cancel(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
@@ -176,27 +132,6 @@ async def action_cancel(message: types.Message, state: FSMContext):
         remove_keyboard = types.ReplyKeyboardRemove()
         await message.answer(f"Упс, что-то пошло не так. Нажмите /reg")
     await state.finish()
-
-
-#@dp.message_handler(state=Form.connect)
-
-
-#dp.message_handler(state=Form.connect)
-#@dp.message_handler(filters.Text("Да", ignore_case=True), state=Form.connect)
-#async def with_puree(message: types.Message, state: FSMContext, callback: types.CallbackQuery):
-#    async with state.proxy() as data:
-#        data['connect'] = message.text
-#    await message.reply("Отправляю в канал")
-#    await bot.send_message(
-#             -1001830422328,
-#             md.text(
-#                 md.text('Заказчик', md.bold(data['name'])),
-#                 md.text('Проект', md.bold(data['projected'])),
-#                 md.text('Способ связи:', data['connect']),
-#                 sep='\n',
-#                 )
-#                 )
-
 
 
 
